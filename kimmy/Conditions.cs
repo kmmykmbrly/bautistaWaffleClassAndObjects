428951f976103ddd97a961366ad024bc22863787
teresa <teresa@ASTRE>
2023-07-15 11:58:19 +0800

    Add project files.

---
+++ kimmy/Conditions.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace kimmy
+{
+    public class Conditions
+    {
+        public void conditionOne()
+        {
+            int numOrder = 1;
+            double total = 0;
+            double finalTotal = 0;
+
+            var conditionOne = new List<string>()
+            {"Plain Waffle = Php30.00", " ", "How many?"};
+
+            foreach (string i in conditionOne)
+            {
+                Console.WriteLine(i);
+            }
+
+            numOrder = Convert.ToInt32(Console.ReadLine());
+            total = numOrder * 30;
+
+            var dineInOrtakeOut = new List<string>()
+            {"Dine in?? or Take out??", "If take out, there is an additional fee of Php5.00", "Type A, if Dine In. Type B, If Take out",
+            " "};
+
+            foreach (string i in dineInOrtakeOut)
+            {
+                Console.WriteLine(i);
+            }
+
+            string cusInput = Console.ReadLine();
+
+            if (cusInput == "A")
+            {
+                Console.WriteLine("Your total bill: Php" + total);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+            if (cusInput == "B")
+            {
+                finalTotal = total + 5;
+                Console.WriteLine("Your total bill: Php" + finalTotal);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+
+            Console.WriteLine("Would you like to order again??");
+            Console.WriteLine("If you like to order again press 'Y', if no press 'N' ");
+        }
+        public void conditionTwo()
+        {
+            int numOrder = 1;
+            double total = 0;
+            double finalTotal = 0;
+
+            var conditionTwo = new List<string>()
+            {"Peanut Waffle = Php40.00", "How many?", " "};
+
+            foreach (string i in conditionTwo)
+            {
+                Console.WriteLine(i);
+            }
+
+            numOrder = Convert.ToInt32(Console.ReadLine());
+            total = numOrder * 40;
+
+            var dineInOrtakeOut = new List<string>()
+            {" ", "Dine in?? or Take out??", "If take out, there is an additional fee of Php5.00", "Type A, if Dine In. Type B, If Take out",
+            " "};
+
+            foreach (string i in dineInOrtakeOut)
+            {
+                Console.WriteLine(i);
+            }
+
+            string cusInput = Console.ReadLine();
+
+            if (cusInput == "A")
+            {
+                Console.WriteLine("Your total bill: Php" + total);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+            if (cusInput == "B")
+            {
+                finalTotal = total + 5;
+                Console.WriteLine("Your total bill: Php" + finalTotal);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+
+            Console.WriteLine("Would you like to order again??");
+            Console.WriteLine("If you like to order again press 'Y', if no press 'N' ");
+        }
+        public  void conditionThree()
+        {
+            int numOrder = 1;
+            double total = 0;
+            double finalTotal = 0;
+
+            var conditionThree = new List<string>()
+            {"Vanilla Waffle = Php45.00", "How many?", " "};
+
+            foreach (string i in conditionThree)
+            {
+                Console.WriteLine(i);
+            }
+
+            numOrder = Convert.ToInt32(Console.ReadLine());
+            total = numOrder * 45;
+
+            var dineInOrtakeOut = new List<string>()
+            {" ", "Dine in?? or Take out??", "If take out, there is an additional fee of Php5.00", "Type A, if Dine In. Type B, If Take out",
+            " "};
+
+            foreach (string i in dineInOrtakeOut)
+            {
+                Console.WriteLine(i);
+            }
+
+            string cusInput = Console.ReadLine();
+
+            if (cusInput == "A")
+            {
+                Console.WriteLine("Your total bill: Php" + total);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+            if (cusInput == "B")
+            {
+                finalTotal = total + 5;
+                Console.WriteLine("Your total bill: Php" + finalTotal);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+
+            Console.WriteLine("Would you like to order again??");
+            Console.WriteLine("If you like to order again press 'Y', if no press 'N' ");
+        }
+        public  void conditionFour()
+        {
+            int numOrder = 1;
+            double total = 0;
+            double finalTotal = 0;
+
+            var conditionFour = new List<string>()
+            {"Chocolate Waffle = Php45.00", "How many?", " "};
+
+            foreach (string i in conditionFour)
+            {
+                Console.WriteLine(i);
+            }
+
+            numOrder = Convert.ToInt32(Console.ReadLine());
+            total = numOrder * 45;
+
+            var dineInOrtakeOut = new List<string>()
+            {" ", "Dine in?? or Take out??", "If take out, there is an additional fee of Php5.00", "Type A, if Dine In. Type B, If Take out",
+            " "};
+
+            foreach (string i in dineInOrtakeOut)
+            {
+                Console.WriteLine(i);
+            }
+
+            string cusInput = Console.ReadLine();
+
+            if (cusInput == "A")
+            {
+                Console.WriteLine("Your total bill: Php" + total);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+            if (cusInput == "B")
+            {
+                finalTotal = total + 5;
+                Console.WriteLine("Your total bill: Php" + finalTotal);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+
+            Console.WriteLine("Would you like to order again??");
+            Console.WriteLine("If you like to order again press 'Y', if no press 'N' ");
+        }
+       public void conditionFive()
+        {
+            int numOrder = 1;
+            double total = 0;
+            double finalTotal = 0;
+
+            var conditionFive = new List<string>()
+            {"Strawberry Waffle = Php55.00", "How many?", " "};
+
+            foreach (string i in conditionFive)
+            {
+                Console.WriteLine(i);
+            }
+
+            numOrder = Convert.ToInt32(Console.ReadLine());
+            total = numOrder * 55;
+
+            var dineInOrtakeOut = new List<string>()
+            {" ", "Dine in?? or Take out??", "If take out, there is an additional fee of Php5.00", "Type A, if Dine In. Type B, If Take out",
+            " "};
+
+            foreach (string i in dineInOrtakeOut)
+            {
+                Console.WriteLine(i);
+            }
+
+            string cusInput = Console.ReadLine();
+
+            if (cusInput == "A")
+            {
+                Console.WriteLine("Your total bill: Php" + total);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+            if (cusInput == "B")
+            {
+                finalTotal = total + 5;
+                Console.WriteLine("Your total bill: Php" + finalTotal);
+                Console.WriteLine("Thank you, have a great day ahead!");
+            }
+
+            Console.WriteLine("Would you like to order again??");
+            Console.WriteLine("If you like to order again press 'Y', if no press 'N' ");
+
+        }
+
+    }
+}
---
+++ kimmy/Program.cs
@@ -0,0 +1,80 @@
+﻿namespace kimmy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numOrder = 1;
+            double total = 0;
+            double finalTotal = 0;
+            string orderAgain;
+            bool Menu = true;
+            while (Menu)
+            {
+                WaffleList waffleList = new WaffleList();
+                waffleList.waffleMenu();
+                string userInput = Console.ReadLine();
+                if (userInput == "1")
+                {
+                    Conditions condition1 = new Conditions();
+                    condition1.conditionOne();
+                    orderAgain = Console.ReadLine();
+                    if (orderAgain != "Y")
+                        Menu = false;
+                    if (orderAgain == "N")
+                    {
+                        Console.WriteLine("OKAY, THANK YOU AND WE HOPE YOU COME AGAIN!!! ");
+                    }
+                }
+                else if (userInput == "2")
+                {
+                    Conditions condition2 = new Conditions();
+                    condition2.conditionTwo();
+                    orderAgain = Console.ReadLine();
+                    if (orderAgain != "Y")
+                        Menu = false;
+                    if (orderAgain == "N")
+                    {
+                        Console.WriteLine("OKAY, THANK YOU AND WE HOPE YOU COME AGAIN!!! ");
+                    }
+                }
+                else if (userInput == "3")
+                {
+                    Conditions condition3 = new Conditions();
+                    condition3.conditionThree();
+                    orderAgain = Console.ReadLine();
+                    if (orderAgain != "Y")
+                        Menu = false;
+                    if (orderAgain == "N")
+                    {
+                        Console.WriteLine("OKAY, THANK YOU AND WE HOPE YOU COME AGAIN!!! ");
+                    }
+                }
+                else if (userInput == "4")
+                {
+                    Conditions condition4 = new Conditions();
+                    condition4.conditionFour();
+                    orderAgain = Console.ReadLine();
+                    if (orderAgain != "Y")
+                        Menu = false;
+                    if (orderAgain == "N")
+                    {
+                        Console.WriteLine("OKAY, THANK YOU AND WE HOPE YOU COME AGAIN!!! ");
+                    }
+                }
+                else if(userInput == "5")
+                {
+                    Conditions condition5 = new Conditions();
+                    condition5.conditionFive();
+                    orderAgain = Console.ReadLine();
+                    if (orderAgain != "Y")
+                        Menu = false;
+                    if (orderAgain == "N")
+                    {
+                        Console.WriteLine("OKAY, THANK YOU AND WE HOPE YOU COME AGAIN!!! ");
+                    }
+                }
+            }
+        }
+    }
+}
